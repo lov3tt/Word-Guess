28798a9d019fc584fb8720db60bba1e1c2020d85
Bobby Quach <BobbyQuach90@gmail.com>
2019-03-30 17:05:04 -0400

    finish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,22 @@
 // Array for hangman
-var PokemonArray = ["pikachu", "raichu", "bulbasaur", "ivysaur", "venusaur","charmander", "charmeleon", "charizard",
-"squirtle", "wartortle", "blastoise", "jigglypuff", "wigglytuff"];
-console.log(PokemonArray);
+var PokemonArray = ["pikachu", "raichu", "bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard",
+    "squirtle", "wartortle", "blastoise", "jigglypuff", "wigglytuff"];
+
 // variable
-var wins =0,
-   loses =0,
-   guess =6;
-   
+var wins = 0;
+var loses = 0;
+var guessRemaining = 0;
+var guessLimit = 7;
+var gameStarted = false;
 
 // variable that hold word choices
-var generate = "";
-var pick= [];
-var placeholder= 0;
+var pickedWord = "";
+var pick = [];
 var userGuess = [];
-var correctGuess= [];
-var wrongGuess= [];
+var correctGuess = [];
+var wrongGuess = [];
 var underscore = [];
 
-
-// //replace the letter with "_" underscore
-// for (var i=0; i<placeholder; i++){
-//     underscore.push("_");
-// }
-//Fxn that pick a word from the array.
-function update () {
-//randomly choose a string from the array
-    generate = PokemonArray[Math.floor(Math.random() * PokemonArray.length)];
-//break up the string in an array
-    pick = generate.split("");
-//make the variable flexible to the length of the randomly chosen word
-    placeholder = pick.length;
-//Add variables inside the fxn
-var underscore = [];
-//Replace the letter with _ via pushing.
-for (var i=0; i<placeholder; i++){
-    underscore.push('_ ');
-}
 //Variable that hold reference in the HTML
 var winsText = document.getElementById("Win-Text");
 var losesText = document.getElementById("Lost-Text");
@@ -43,48 +24,100 @@ var guessText = document.getElementById("Guess-Text");
 var wrongGuessText = document.getElementById("Userguess-Text");
 var underscoreText = document.getElementById("Display");
 var correctGuessText = document.getElementById("");
-//Display the stats on the HTML
-winsText.textContent = "Wins: "+wins;
-losesText.textContent = "Losses: "+loses;
-guessText.textContent = "Guesses Remaining: " + guess;
-wrongGuessText.textContent ="Guesses"+ wrongGuess;
-underscoreText.textContent = underscore;
-//Something is broken here
-document.onkeyup = function(event){
-    userGuess = event.key.toLowerCase();
-//copy&paste Convert to only letters response, but how?
-function onlyAlphabets(e, t) {
-    try {
-        if (window.event) {
-            var charCode = window.event.keyCode;
-        }
-        else if (e) {
-            var charCode = e.which;
-        }
-        else { return true; }
-        if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123))
-            return true;
-        else
-            return false;
-    }
-    catch (err) {
-        alert("Please choose letters only");
-    }
-} 
-//Logic that determine if part of the letter is guess right or wrong
 
-//2nd attempt at Logic
 
+//figure out to make it run auto
+function update(guess) {
+
+    //The user has not previously guessed the letter.
+    if (correctGuess.indexOf(guess) == -1 && wrongGuess.indexOf(guess) == -1) {
+
+        //User guessed incorrectly
+        if (pickedWord.indexOf(guess) == -1) {
+            wrongGuess.push(guess);
+            guessRemaining--;
+            //gameover & restart
+            if (0 >= guessRemaining) {
+                loses++
+                alert("You lose!");
+                
+                restart();
+            }
+        }
+        //user guessed correct
+        else {
+            //replace the _ with the correct guess letter
+            correctGuess.push(guess);
+            //game won & restart: When the value of _ no longer exist in the whatToDraw fxn, add+1,alert, and restart
+            if (whatToDraw().indexOf("_") == -1) {
+                wins++
+                alert("You win! The pokemon was: " + pickedWord)
+
+                restart();
+            }
+        }
+    }
+    else {
+        alert("You have already guessed letter: " + guess + ", please guess another.")
+    }
+
+    winsText.textContent = "Wins: " + wins;
+    losesText.textContent = "Losses: " + loses;
+    guessText.textContent = "Guesses Remaining: " + guessRemaining;
+    wrongGuessText.textContent = "Guesses: " + wrongGuess;
+    underscoreText.textContent = whatToDraw();
 }
 
-console.log(userGuess);
-console.log(pick);
-console.log(generate);
-console.log(placeholder);
-console.log(underscore);
+function restart() {
+    guessRemaining = guessLimit;
+
+    wrongGuess = [];
+    correctGuess = [];
+    underscore = [];
+    userGuess = [];
+
+    //randomly choose a string from the array
+    pickedWord = PokemonArray[Math.floor(Math.random() * PokemonArray.length)];
 
 
+
+    update();
 }
-update()
+//Draw in _ base on picked word length
+function whatToDraw() {
+    
+    var stringToDraw = "";
 
+    for(var i = 0; i < pickedWord.length; i++){
+        var currentCharacter = pickedWord[i];
 
+        if(correctGuess.indexOf(currentCharacter) != -1){
+            stringToDraw += currentCharacter + " ";
+//replace the letters with _
+        }
+        else{
+            stringToDraw += "_ ";
+        }
+    }
+
+    return stringToDraw;
+}
+
+//User keypress that only account for letters
+document.onkeyup = function (event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90 && gameStarted) {
+        //Get the character as lowercase for comparison simplicity.
+        userGuess = event.key.toLowerCase();
+
+        update(userGuess);
+    }
+
+    //
+    else if(gameStarted == false){
+        gameStarted = true;
+        restart();
+    }
+    else {
+        alert("Please guess a letter, other characters are not allowed.")
+    }
+}
